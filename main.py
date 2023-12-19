112a66b6f65d16780c0c5d5ff230c6fc6f628135
j-zeringue <jmz6031@gmail.com>
2023-12-19 14:25:52 -0600

    built out the save function and added some error handling

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ run = True
 menu = True
 play = False
 rules = False
+key = False
 
 hp = 50
 atk = 3
+potions = 1
+elixirs = 0
+gold = 0
+x = 0
+y = 0
 
 def clear():
     os.system("cls")
@@ -16,6 +22,12 @@ def save():
         name, 
         str(hp),
         str(atk),
+        str(potions),
+        str(elixirs),
+        str(gold),
+        str(x),
+        str(y),
+        str(key)
     ]
 
     f = open('load.txt,', 'w')
@@ -26,6 +38,7 @@ def save():
 
 while run:
     while menu:
+        clear()
         print('1. NEW GAME')
         print('2. LOAD GAME')
         print('3. RULES')
@@ -40,19 +53,34 @@ while run:
         choice = input(': ')
 
         if choice == '1':
+            clear()
             name = input('What is your name? ')
             menu = False
             play = True
         if choice == '2':
-            f = open('load.txt', 'r')
-            save_file = f.readlines
-            name = save_file[0]
-            hp = save_file[1]
-            atk = save_file[2]
-            print(f'welcome back {name} to your adventure!')
-            input(': ')
-            menu = False
-            play = True
+            try:
+                f = open('load.txt', 'r')
+                save_file = f.readlines
+                if len(save_file) == 9:
+                    name = save_file[0]
+                    hp = int(save_file[1])
+                    atk = int(save_file[2])
+                    potions = int(save_file[3])
+                    elixirs = int(save_file[4])
+                    gold = int(save_file[5])
+                    x = int(save_file[6])
+                    y = int(save_file[7])
+                    key = bool(save_file[8])
+                    clear()
+                    print(f'welcome back {name} to your adventure!')
+                    input(': ')
+                    menu = False
+                    play = True
+                else:
+                    print('File is corrupt')
+            except OSError:
+                print("No save file exists")
+                input(': ')
         if choice == '3':
             rules = True
         if choice == '4':
